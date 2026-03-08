5ab14900624cc4851771ac195d233ac73513e20e
omardtl24 <otoledo@unal.edu.co>
2026-03-08 16:16:28 -0500

    fix: general text capture

--- src/bankparser/parser/nequi_parser.py
+++ src/bankparser/parser/nequi_parser.py
@@ -1,7 +1,7 @@
 from .base_parser import Parser
 from bankparser.loader.pdf_loader import PDFLoader
 
-DATA_PATTERN = r'\s*(\d{2}/\d{2}/\d{4})\s*([A-Za-záéíóúÁÉÍÓÚ0-9:\-_\.,\s]+)\s*\$(-?[\d,\.]+)\s*\$(-?[\d,\.]+)\s*'
+DATA_PATTERN = r'\s*(\d{2}/\d{2}/\d{4})\s*(.+)\s*\$(-?[\d,\.]+)\s*\$(-?[\d,\.]+)\s*'
 WALLET_COLS_GROUPS_IDS = {"date": 1, "description": 2, "amount": 3, "balance": 4}
 WALLET_DEFAULT_VALUES = {"account": "Nequi", "currency": "COP"}
 
